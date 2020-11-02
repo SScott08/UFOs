7850f6aa8b303d3330b833528e3a5fc5b2000232
SScott08 <68204195+SScott08@users.noreply.github.com>
2020-11-01 20:28:54 -0800

    website

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,10 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
-
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
-  
+  console.log(data)
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
@@ -27,7 +26,7 @@ function buildTable(data) {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-  
+  console.log(filteredData)
      // Check to see if a date was entered and filter the
     // data using that date.
     if (date) {
